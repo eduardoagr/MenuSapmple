7442b3fa5265f0f9d337f2efa25dda2d85f50e69
Grégoire Chapeaux <gregoire.chapeaux@apexo.net>
2024-03-26 16:22:02 -0400

    Fix bug COmmand

--- View/MainPage.xaml.cs
+++ View/MainPage.xaml.cs
@@ -3,7 +3,8 @@ namespace MenuSapmple.View;
 public partial class MainPage : ContentPage {
 
     public MainPage(MainViewModel mainViewModel) {
-        InitializeComponent();
         BindingContext = mainViewModel;
+        InitializeComponent();
+        YourMainTemplate.UpdateMenu();
     }
 }
